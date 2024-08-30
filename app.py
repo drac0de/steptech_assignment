2bee74fd7a0dcc822dffa4dbe8564fefa8a8e09a
drac0de <ashishverma527@gmail.com>
2024-08-30 19:02:47 +0530

    added route /users and /new_user

--- app.py
+++ app.py
@@ -19,5 +19,22 @@ def home():
 def hello():
     return "Hello, World!"
 
+@app.route('/users')
+def users():
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+    return render_template('users.html', users=users)
+
+@app.route('/new_user', methods=['GET', 'POST'])
+def new_user():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        role = request.form['role']
+        cursor.execute("INSERT INTO users (name, email, role) VALUES (%s, %s, %s)", (name, email, role))
+        db.commit()
+        return redirect(url_for('users'))
+    return render_template('new_user.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
